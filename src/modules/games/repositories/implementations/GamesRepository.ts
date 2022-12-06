212467e282232d08fb91905a6a3eafbe51817a29
Iann-rst <iannrst11@gmail.com>
2022-12-06 17:36:19 -0300

    feat: desafios

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,19 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder().where('title ILIKE :title', { title: `%${param}%` }).getMany()
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      'SELECT count(*) from games'
+    ); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder().relation(Game, "users").of(id).loadMany();
+    // Complete usando query builder
   }
 }
